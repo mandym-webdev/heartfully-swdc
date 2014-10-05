ad47044f504054b6f964ccad30f46e76569bf2eb
mandym_webdev <mandy@mandym.com>
2014-10-05 13:26:58 -0400

    updated typo and seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ Project.create([
     project_type: "Children/Family",
     project_description:"A Tanzanian Non-governmental organization (Foxes NGO) was registered with the aim to provide shelter, sustenance, education, and medical care for children in Mufindi, and to curtail the spread of HIV. Mufindi Orphans was founded shortly after in the United States as a 501(c)3 registered charity.",
     project_amount: 5000,
-    project_pic: "http://mandym.com/training/wp-content/uploads/2014/10/projectmufundilogo.png"
+    project_pic: "http://mandym.com/training/wp-content/uploads/2014/10/projectpagepic.png"
   },
   {
     project_name: "SEM Fund",
@@ -22,7 +22,7 @@ Project.create([
     project_type: "Environment/Energy",
     project_description:"Our aim is to create a world-class organization and brand known for positive change by making accessible improved energy products and services for low-income communities throughout Senegal. To tackle health issues, environmental degradation and poverty, SEM is creating viable solutions for low-income communities to satisfy their basic needs in a safer, cheaper and environmentally friendly way. ",
     project_amount: 3000,
-    project_pic: "http://mandym.com/training/wp-content/uploads/2014/10/Project4Logo.png"
+    project_pic: "http://mandym.com/training/wp-content/uploads/2014/10/projectpagepic2.png"
   },
   {
     project_name: "Solar Aid",
@@ -31,6 +31,6 @@ Project.create([
     project_description:"SolarAid is a leading international charity that provides access to clean, affordable solar lights.\n
       Based in London and already working in Kenya, Malawi, Tanzania and Zambia we have set ourselves an ambitious goal: to eradicate the kerosene lamp from Africa by 2020.",
     project_amount: 7000,
-    project_pic: "http://mandym.com/training/wp-content/uploads/2014/10/Project3Logo.png"
+    project_pic: "http://mandym.com/training/wp-content/uploads/2014/10/projectpagepic3.png"
   }
   ])
